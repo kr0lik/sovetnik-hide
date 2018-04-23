7b279bebe58676943660b661757d83a95bb3d884
Denis <krolik@yandex.ru>
2018-04-23 16:16:51 +0300

    Update stopsovetnik.js

--- src/stopsovetnik.js
+++ src/stopsovetnik.js
@@ -1,3 +1,25 @@
+(function(){
+    function findByAttributeValue(attribute, value, element_type)    {
+        element_type = element_type || "*";
+        var All = document.getElementsByTagName(element_type);
+        for (var i = 0; i < All.length; i++)       {
+            if (All[i].getAttribute(attribute) == value) { return All[i]; }
+        }
+
+        return [];
+    }
+
+    ['itemprop', 'itemscope', 'itemtype'].forEach(function(element) {
+        var elements = document.querySelectorAll('[' + element + ']');
+
+        elements.forEach(function(item, i, arr) {
+            item.removeAttribute(element);
+        });
+    });
+
+    $('h1').before('<h1 style="display: none;">Stop sovetnik</h1>');
+})();
+
 function sovetnikFix() {
 	var el = document.getElementsByTagName('html')[0];
 	if (el.style && el.style.marginTop && el.style.marginTop != '0px') {
